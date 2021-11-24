e2a86ee45b3013bd3b5737ca395d3ff3c8abf70b
karolz32 <karoline.lima32@gmail.com>
2021-11-24 20:19:27 -0300

    modifica class OrdemInversa

--- src/br/com/dio/arrays/OrdemInversa.java
+++ src/br/com/dio/arrays/OrdemInversa.java
@@ -1,17 +1,24 @@
 package br.com.dio.arrays;
 
+import java.util.Scanner;
+
 public class OrdemInversa {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        int[] vetor = {6, -8, 19, 25, 1, 65};
+        int[] vetor = new int[5];
 
-        System.out.print("Vetor: ");
+
         int count = 0;
         while(count < (vetor.length)){
-            System.out.print(vetor[count] + " ");
+
+            System.out.println("Digite número: ");
+            int numero = scan.nextInt();
+            vetor[count] = numero;
             count++;
         }
-        System.out.print("\nVetor: ");
+
+        System.out.print("\nVetor invertido: ");
         for (int i = (vetor.length-1); i >= 0; i--){
             System.out.print(vetor[i] + " ");
         }
